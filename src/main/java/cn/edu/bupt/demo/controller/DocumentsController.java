335fd058d37a243e6c31112c11ac2557bb0c9e3d
zy <yi950607@126.com>
2018-12-07 19:01:21 +0800

    增加下载和删除文件接口

--- src/main/java/cn/edu/bupt/demo/controller/DocumentsController.java
+++ src/main/java/cn/edu/bupt/demo/controller/DocumentsController.java
@@ -1,12 +1,15 @@
 package cn.edu.bupt.demo.controller;
 
+import org.apache.tomcat.util.http.fileupload.IOUtils;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.File;
-import java.io.IOException;
-import java.io.UnsupportedEncodingException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.*;
 import java.net.URLDecoder;
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
 
 /**
  * @author zy
@@ -17,6 +20,9 @@ import java.net.URLDecoder;
 @RequestMapping("/api/v1/info")
 @CrossOrigin
 public class DocumentsController {
+
+//    String storePath= "/home/zy/file";//存放我们上传的文件路径
+    String storePath = "/Users/zy/Desktop/file";
 
     @RequestMapping(value = "/upload", method = RequestMethod.POST)
     public String uploadFile(@RequestParam("file") MultipartFile file) throws Exception{
@@ -29,8 +35,15 @@ public class DocumentsController {
             String fileName = file.getOriginalFilename();
             fileName = URLDecoder.decode(fileName,"UTF-8");
             String suffixName = fileName.substring(fileName.lastIndexOf("."));
-            String filePath = "/home/zy/file";
-            String path = filePath +"/"+ fileName;
+
+            File filePath = new File(storePath, fileName);
+            if (!filePath.getParentFile().exists()) {
+
+                filePath.getParentFile().mkdirs();//如果目录不存在，创建目录
+
+            }
+
+            String path = storePath+File.separator+fileName;
             File dest = new File(path);
             file.transferTo(dest);// 文件写入
             return "上传成功";
@@ -42,11 +55,48 @@ public class DocumentsController {
         return "上传失败";
     }
 
+
+    @RequestMapping(value = "/download/{filename}/{fileType}", method = RequestMethod.GET)
+    public void downloadFile(@PathVariable("filename") String filename,
+                             @PathVariable("fileType") String fileType,
+                             HttpServletResponse response,
+                             HttpServletRequest request) throws IOException {
+        response.setCharacterEncoding(request.getCharacterEncoding());
+        response.setContentType("application/octet-stream");
+        filename = URLDecoder.decode(filename,"UTF-8");
+        FileInputStream fis = null;
+        try {
+            File file = new File(storePath+"/"+filename+"."+fileType);
+            fis = new FileInputStream(file);
+            response.setHeader("charset", "utf-8");
+            String encodeName = URLEncoder.encode(file.getName(), StandardCharsets.UTF_8.toString());
+            response.setHeader("Content-Disposition", "attachment; filename="+encodeName);
+            IOUtils.copy(fis,response.getOutputStream());
+            response.flushBuffer();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (fis != null) {
+                try {
+                    fis.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+    }
+
     @RequestMapping(value = "/delete/{fileName}/{fileType}", method = RequestMethod.DELETE)
-    public void deleteFile(@PathVariable("fileName") String fileName, @PathVariable("fileType") String fileType) throws UnsupportedEncodingException {
-        //fileName = URLEncoder.encode(fileName, StandardCharsets.UTF_8.toString());
-        System.out.println("enter");
-        File file = new File("/root/doc/"+fileName+"."+fileType);
+    public void deleteFile(@PathVariable("fileName") String fileName,
+                           @PathVariable("fileType") String fileType) throws UnsupportedEncodingException {
+
+        System.out.println("name1"+fileName);
+//        fileName = URLEncoder.encode(fileName, StandardCharsets.UTF_8.toString());
+//        fileName=new String(fileName.getBytes("iso8859-1"),"UTF-8");
+        File file = new File(storePath+"/"+fileName+"."+fileType);
         System.out.println(file.getName()+"|"+file.exists());
         if(file.exists()){
             System.out.println(file.delete());
