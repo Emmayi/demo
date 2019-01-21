a04f4da1fa3ffedb8664d05b987f89ddd1571021
zy <yi950607@126.com>
2019-01-21 16:49:58 +0800

    修改数据库为内网ip

--- src/main/java/cn/edu/bupt/demo/DemoApplication.java
+++ src/main/java/cn/edu/bupt/demo/DemoApplication.java
@@ -4,8 +4,12 @@ import org.apache.ibatis.annotations.Mapper;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ImportResource;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
+@PropertySource({"classpath:disconf.properties"})
+@ImportResource({"classpath:disconf.xml"})//引入disconf
 public class DemoApplication {
 
 	public static void main(String[] args) {
