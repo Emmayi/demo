cdb7641403d8683da78c57a98b1fbdee1c58121f
feeli <feeli@tencent.com>
2020-03-31 19:31:00 +0800

    debug device-access

--- src/main/java/cn/edu/bupt/demo/controller/DeviceController.java
+++ src/main/java/cn/edu/bupt/demo/controller/DeviceController.java
@@ -57,7 +57,7 @@ public class DeviceController {
         ClientTask tc = new ClientTask() {
             @Override
             public Object call() throws Exception {
-                return JSONArray.parseArray(this.getResp().getBody().toString(),JSONArray.class);
+                return JSONArray.parse(this.getResp().getBody().toByteArray());
             }
         };
         ClientFutureTask dt = new ClientFutureTask(tc);
